02f4f5f1bed325188eacfc0ce07bcf98aa4044fa
Ty Christensen <ty.christensen@wpengine.com>
2022-10-20 19:49:34 -0400

    progress

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -40,6 +40,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(numApples: u32) -> u32 { 
+    if numApples <= 40 {
+        return numApples * 2
+    }
+    return numApples
+}
 
 // Don't modify this function!
 #[test]
