75afa6a4372d94d41f7f096c39d3ccd7633066e8
Ty Christensen <tychristensen97@gmail.com>
2022-11-28 08:49:42 -0500

    prog prog prog

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -23,7 +23,13 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    } else if a % b != 0 {
+        return Err(DivisionError::NotDivisible(NotDivisibleError { dividend: a, divisor: b }));
+    }
+
+    Ok(a / b)
 }
 
 // Complete the function and return a value of the correct type so the test passes.
@@ -31,6 +37,8 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 fn result_with_list() -> () {
     let numbers = vec![27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    
+    division_results.collect::<Vec<i32>>()
 }
 
 // Complete the function and return a value of the correct type so the test passes.
